3b3c319f353732067b945a2779e3140f77458ff7
Wallace <wallaceguimaraes7@outlook.com>
2022-01-20 03:33:08 -0300

    Segundo planejamento da regra de negocio

---
+++ Models/ServiceModel/Services/AdvanceRequest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models.ServiceModel.Services
+{
+    public class AdvanceRequest
+    {
+        /*
+        SOLICITAÇÕES DE ANTECIPAÇÃO SÃO DOCUMENTOS EMITIDOS PELO LOJISTA/VENDEDOR ATRAVÉS DO
+        NOSSO SERVIÇO DE REPASSE ANTECIPADO DE  VALORES. A antecipação de uma transação tem um 
+        CUSTO de 3.8% APLICADO em CADA PARCELA da TRANSAÇÃO, se APROVADA pela análise do financeiro, 
+        sendo automaticamente debitado no seu repasse. Considerando o exemplo da transação citado na 
+        fase 1, se cada parcela da transação tem valor líquido de 49,55, o valor antecipado da parcela 
+        seria obtido a partir desse valor líquido, debitado a taxa de 3.8%.
+
+        CRITÉRIOS DE ACEITAÇÃO
+
+        1° - Não é permitido incluir em uma NOVA SOLICITAÇÃO DE ANTECIPAÇÃO de transações solicitadas anteriormente;
+        Para realização de uma nova solicitação de antecipação, é necessário que a solicitação atual já tenha sido FINALIZADA;
+
+        2° - A data de finalização da análise deve ser preenchida quando todas as transações da antecipação forem resolvidas 
+        como aprovadas ou reprovadas;
+
+        3° - Aplicar taxa de 3.8% em cada parcela de transação antecipada, considerando o valor líquido da parcela. Esse valor deve 
+        ser armazenado no campo "Valor antecipado" da parcela da transação em questão;
+
+
+        4° - Caso a transação seja aprovada na antecipação, ao finalizar a solicitação, deve ter o campo "Data em que a parcela 
+        foi repassada", da entidade "Parcela", preenchida com a data atual.
+
+
+        FLUXO DO PROCESSO
+
+        5° - O trâmite de uma solicitação de antecipação progride através das seguintes etapas:
+
+        Aguardando análise (PENDENTE): O lojista solicitou antecipação, mas ainda não foi INICIADO a análise pela equipe 
+        financeira da Pagcerto;
+
+        EM ANÁLISE: A equipe financeira iniciou a análise da antecipação, podendo aprovar ou reprovar UMA ou MAIS 
+        TRANSAÇÕES contidas na SOLICITAÇÃO;
+
+        FINALIZADA: Quando a análise da solicitação é encerrada, a antecipação pode assumir um dos seguintes resultados: 
+        aprovada (todas as transações aprovadas), 
+        aprovada parcialmente (quando existe ao menos uma transação aprovada e uma transação reprovada na análise) 
+        ou reprovada (todas as transações reprovadas).
+
+        ROTAS
+
+        1° Endpoint -> Consultar transações disponíveis para solicitar antecipação (não é necessário filtros);
+        2° Endpoint -> Solicitar antecipação a partir de uma lista de transações;
+        3° Endpoint -> Iniciar o atendimento da antecipação;
+        4° Endpoint -> Aprovar ou reprovar uma ou mais transações da antecipação (quando todas as transações forem finalizadas, 
+        a antecipação será finalizada);
+        5° Endpoint -> Consultar histórico de antecipações com filtro por status (pendente, em análise, finalizada).
+
+        */
+
+    }
+}
--- Models/ServiceModel/Services/PaymentService.cs
+++ Models/ServiceModel/Services/PaymentService.cs
@@ -24,18 +24,49 @@ namespace api.Models.ServiceModel.Services
         public async Task<IActionResult> MakePayment(PaymentModel vModel)
         {
             /*
-            Toda transação aprovada deve gerar parcelas com vencimento a cada 30 dias, 
+
+
+            CRITÉRIOS DE ACEITAÇÃO
+
+            1° - Cobrar taxa fixa de 0,90 nas transações aprovadas;
+            Na requisição de transação (efetuar pagamento), o número do cartão deve conter 16 caracteres numéricos, sem espaços;
+
+            2° - Caso o número do cartão inicie com "5999", deve ter a transação REPROVADA ao EFETUAR PAGAMENTO, nos demais casos 
+            válidos a transação deverá ser aprovada; GERAR PARCELAS apenas em TRANSAÇÕES aprovadas;
+
+
+            GERAR PARCELAS (Portion)
+
+            3° - Toda transação aprovada deve gerar parcelas com vencimento a cada 30 dias, 
             exemplo: Se a transação for de R$100,00 em 2x (duas parcelas), deve ser criado o 
             registro de transação (entidade FORTE), conforme os critérios acima, e uma lista de 
             parcelas associadas a essa transação (entidade FRACA). 
 
+            CALCULAR QUANTIDADE DE PARCELAS POR 30 PARA GERAR VENCIMENTO DE CADA PARCELA
+            EX: PARCELA 1 VENCE COM 30 DIAS, POIS 1 X 30 = 30
+                PARCELA 2 VENCE COM 60 DIAS, POIS 2 X 30 = 60
+                PARCELA 3 VENCE COM 90 DIAS, POIS 3 X 30 = 90   
 
+            4° - O vencimento de cada parcela deverá ser obtido através da MULTIPLICAÇÃO do número da parcela por 30, conforme 
+            exemplificado acima;
+            O valor LÍQUIDO da parcela deverá ser obtido a partir do valor BRUTO da TRANSAÇÃO subtraído a TAXA FIXA, 
+            dividido pelo número de parcelas (já citado em exemplo).
+
+
+            5° - DIVIDIR VALOR TOTAL SUBTRAIDO (TAXA FIXA) POR QUANTIDADE DE PARCELAS
 
             Nesse exemplo, seriam geradas duas parcelas de R$49,55 cada, sendo esse valor obtido 
             a partir do valor da transação, nesse caso 100 reais, subtraido a taxa fixa, 0,90, e 
             dividido pelo número de parcelas, no exemplo 2x. Sobre a data de recebimento das parcelas, 
             ainda nesse exemplo, a primeira teria seu repasse realizado com 30 dias após a data de realização da 
             venda e a segunda parcela com 60 dias a partir da mesma data de referência.
+
+            ROTAS A CRIAR
+
+            1° Endpoint ->  Efetuar pagamento com cartão de crédito (make-payment);
+            2° Endpoint -> Consultar uma transação e suas parcelas a partir do  (consult-transaction)
+           
+
             */
 
 
